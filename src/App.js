f2ffde599b8a23a90d546decb871fb0c752d2c5d
maheshram216 <maheshram216@gmail.com>
2022-11-04 22:36:31 +0530

    added souce code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import TotalContacts from "./pages/TotalContacts";
+import { AuthContextProvider } from "./context/AuthContext";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AuthContextProvider>
+        <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/signup" element={<Signup />} /> 
+            <Route path='/contacts' element={<TotalContacts/>} />
+     
+        </Routes>
+      </AuthContextProvider>
+      
+     
+
+
     </div>
   );
 }
